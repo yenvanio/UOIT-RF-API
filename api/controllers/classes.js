6467e6356e52d09fe9f1936307feb10ce06dd3d2
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2018-09-24 22:26:55 -0400

    lint wip

--- api/controllers/classes.js
+++ api/controllers/classes.js
@@ -11,37 +11,62 @@ const logic = require('../logic/classes.js');
 router.use(bodyParser.urlencoded({ extended: true }));
 
 /**
+ * Converts the day of the week from integer to string
+ * @param {Number} num
+ */
+function convertToDay(num) {
+  switch (num) {
+    case 0:
+      return 'S';
+    case 1:
+      return 'M';
+    case 2:
+      return 'T';
+    case 3:
+      return 'W';
+    case 4:
+      return 'R';
+    case 5:
+      return 'F';
+    case 6:
+      return 'S';
+    default:
+      return null;
+  }
+}
+
+/**
  * Get classes based on query parameters
  * @param {Any} req
  * @param {Any} res
  */
-router.get('/all', function (req, res) {
+function getAll(req, res) {
   // Validating Date Format
   const date = req.query.date || moment().format('YYYY-MM-DD');
-  if (!moment(date,'YYYY-MM-DD').isValid()) {
+  if (!moment(date, 'YYYY-MM-DD').isValid()) {
     res.statusCode = 400;
     res.json({ errors: ['Invalid Date Format! Use (YYYY-MM-DD)'] });
   }
-
   // Validating Time Format
-  var start_time = req.query.start_time || moment().format("HH:mm:ss");
-  var end_time = req.query.end_time || moment(date + ' ' + start_time).add(1, 'hours').format("HH:mm:ss");
+  const startTime = req.query.start_time || moment().format('HH:mm:ss');
+  const space = ' ';
+  const endTime = req.query.end_time || moment(date + space + startTime).add(1, 'hours').format('HH:mm:ss');
 
   // Gathering Parameters
-  var day = convertToDay(moment(date).day()); // Get day from moment
-  var building = req.query.building || '';
+  const day = convertToDay(moment(date).day()); // Get day from moment
+  const building = req.query.building || '';
 
   // Assemble the query data in JSON
-  var queryData = {
-      day: day,
-      date: date,
-      start_time: start_time,
-      end_time: end_time,
-      building: building
-  }
-  
+  const queryData = {
+    day,
+    date,
+    startTime,
+    endTime,
+    building,
+  };
+
   // Retrieve data from the Database
-  logic.getClassesByParam(queryData, function (err, queryResult) {
+  logic.getClassesByParam(queryData, function(err, queryResult) {
     console.log(queryResult);
     if (err) {
       res.statusCode = 500;
@@ -51,7 +76,10 @@ router.get('/all', function (req, res) {
       res.json({ classes: queryResult });
     }
   });
-});
+}
+
+router.get('/all', getAll());
+
 
 /**
  * Get future classes in a room given a class
@@ -93,30 +121,6 @@ router.get('/future', function (req, res) {
     }
   });  
 });
-
-/**
- * Converts the day of the week from integer to string
- * @param {Number} num 
- */
-function convertToDay(num) {
-  console.log(num);
-  switch(num) {
-    case 0: 
-      return 'S'
-    case 1:
-      return 'M'
-    case 2: 
-      return 'T'
-    case 3:
-      return 'W'
-    case 4: 
-      return 'R'
-    case 5:
-      return 'F'
-    case 6:
-      return 'S'
-  }
-}
 
 // Add functions for classes module to export
 module.exports = router;
--- api/logic/classes.js
+++ api/logic/classes.js
@@ -1,20 +1,20 @@
-var db = require('../../config/db');
+const db = require('../../config/db');
 
-  // Connect to mysql database
-  db.initialize(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
+// Connect to mysql database
+db.initialize(function (err) {
+  if(err) {
+    throw err;
+  }
+});
 
- /**
-  * Get Classes based on parameters
-  * @param {JSON Object} data 
-    * day, date, start_time, end_time, building 
-  * @param {Callback} callback 
-  */
-  getClassesByParam = function (data, callback) {
-      var sql = `
+/**
+ * Get Classes based on parameters
+ * @param {JSON Object} data
+ * day, date, start_time, end_time, building 
+ * @param {Callback} callback 
+ */
+getClassesByParam = function (data, callback) {
+  const sql = `
       SELECT DISTINCT(class.room), building.name AS building, building.location, course.isLab FROM class 
           LEFT JOIN course ON class.fk_course_crn = course.crn
           LEFT JOIN building ON class.fk_building_id = building.id
@@ -28,26 +28,23 @@ var db = require('../../config/db');
                         (class.end_time >= '${data.start_time}' AND class.end_time <= '${data.end_time}')
                       GROUP BY room )`
 
-      console.log(sql);
+  db.query(sql, function (err, res) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  });
+}
 
-      db.query(sql, function(err, res) {
-        if (err) {
-          callback(err, null);
-        } 
-        else {
-          callback(null, res);
-        }
-      });
-  }
-
-  /**
-  * Get Future Classes given a Class 
-  * @param {JSON Object} data 
-    * day, date, start_time, room
-  * @param {Callback} callback 
-  */
-  getFutureClasses = function (data, callback) {
-    var sql = `
+/**
+ * Get Future Classes given a Class 
+ * @param {JSON Object} data 
+ * day, date, start_time, room
+ * @param {Callback} callback 
+ */
+getFutureClasses = function (data, callback) {
+  var sql = `
     SELECT class.room, class.start_time, class.end_time, course.title, course.code FROM class 
         LEFT JOIN course ON class.fk_course_crn = course.crn
               WHERE class.day = '${data.day}'
@@ -55,18 +52,20 @@ var db = require('../../config/db');
               AND '${data.date}' >= class.start_date AND '${data.date}' <= class.end_date
               AND '${data.start_time}' <= class.start_time
         ORDER BY class.start_time ASC`;
-  
-    console.log(sql);
 
-    db.query(sql, function(err, res) {
-      if (err) {
-        callback(err, null);
-      } 
-      else {
-        callback(null, res);
-      }
-    });
-  }
+  console.log(sql);
+
+  db.query(sql, function (err, res) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  });
+}
 
 // Add functions for classes module to export
-module.exports = {getClassesByParam, getFutureClasses};
+module.exports = {
+  getClassesByParam,
+  getFutureClasses
+};
